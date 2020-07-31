f54b758ed46cbd31282f410962923dce3a00fb5c
Harnam Singh <harnamsinghsandhu04@gmail.com>
2020-08-01 00:11:35 +0530

    added kky line breaking

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ kky = open(sys.argv[1], "r")
 f = kky.read()
 kky.close()
 print(f)
+
+s = []
+word = ""
+for x in f:
+    if x == '.':
+        s.append(word)
+        word = ""
+    else:
+        word += x
+
+print(s)
